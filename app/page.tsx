9a319a801a103b98e76f2b689ec3d2ae030382b1
Matt Greer <matt.e.greer@gmail.com>
2024-10-31 10:30:26 -0400

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,26 @@
+import Image from "next/image";
+import retroLogoPng from "./retroLogo.png";
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null;
+  return (
+    <div>
+      <div>
+        <p>image loaded with next/image</p>
+        <Image
+          src={retroLogoPng}
+          style={{ imageRendering: "pixelated", width: "80%", height: "auto" }}
+          alt="image loaded with next/image"
+        />
+      </div>
+      <div>
+        <p>image loaded with native img element</p>
+        <img
+          src={retroLogoPng.src}
+          style={{ imageRendering: "pixelated", width: "80%", height: "auto" }}
+          alt="image loaded with native img element"
+        />
+      </div>
+    </div>
+  );
 }
